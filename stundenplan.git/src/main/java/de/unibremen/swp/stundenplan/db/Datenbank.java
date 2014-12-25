bf41acf8e0b48176c3acafc6e0544212472c9443
FreakyAsian <k.long24@gmx.de>
2014-12-25 14:07:46 +0100

    DB aktualisiert

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/db/Datenbank.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/db/Datenbank.java
@@ -9,12 +9,12 @@ public class Datenbank {
 	    String sql;
 	    try {
 	    	Class.forName("org.sqlite.JDBC");
-	    	c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	    	c = DriverManager.getConnection("jdbc:sqlite:sp.db");
 	    	c.setAutoCommit(false);
 		    System.out.println("Opened database successfully");
 		    
 	    	stmt = c.createStatement();
-	    	sql = "CREATE TABLE Personal IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS Personal "
 	    			+ "(id INT PRIMARY KEY NOT NULL, "
 	    			+ "name VARCHAR NOT NULL, "
 	    			+ "kuerzel VARCHAR NOT NULL, "
@@ -24,42 +24,55 @@ public class Datenbank {
 	    			+ "schonGependelt BOOLEAN NOT NULL, "
 	    			+ "lehrer BOOLEAN NOT NULL)";
 	    	stmt.executeUpdate(sql);
-	    	sql = "CREATE TABLE Planungseinheit IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS Planungseinheit "
 	    			+ "(id INT PRIMARY KEY NOT NULL, "
 	    			+ "weekday INT NOT NULL, "
 	    			+ "startTimeslot INT NOT NULL, "
 	    			+ "endTimeslot INT NOT NULL)";
 	    	stmt.executeUpdate(sql);
-	    	sql = "CREATE TABLE Schulklasse IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS Schulklasse "
 	    			+ "(id INT PRIMARY KEY NOT NULL, "
 	    			+ "jahrgang INT NOT NULL, "
 	    			+ "klassenraumId INT NOT NULL)";
 	    	stmt.executeUpdate(sql);
-	    	sql = "CREATE TABLE Raum IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS Raum "
 	    			+ "(id INT PRIMARY KEY NOT NULL, "
 	    			+ "name VARCHAR NOT NULL, "
 	    			+ "kuerzel VARCHAR NOT NULL, "
 	    			+ "gebaeudennr INT NOT NULL)";
 	    	stmt.executeUpdate(sql);
-	    	sql = "CREATE TABLE Stundeninhalt IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS Stundeninhalt "
 	    			+ "(id INT PRIMARY KEY NOT NULL, "
 	    			+ "name VARCHAR NOT NULL, "
 	    			+ "kuerzel VARCHAR NOT NULL, "
 	    			+ "regeldauer INT NOT NULL, "
 	    			+ "rhythmustyp INT NOT NULL)";
 	    	stmt.executeUpdate(sql);
-	    	sql = "CREATE TABLE Zeitwunsch IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS Zeitwunsch "
 	    			+ "(personal_id INT NOT NULL, "
 	    			+ "weekday INT NOT NULL, "
 	    			+ "startZeit INT NOT NULL, "
-	    			+ "endZeit INT NOT NULL"
+	    			+ "endZeit INT NOT NULL, "
 	    			+ "PRIMARY KEY (personal_id, weekday)"
-	    			+ "FOREIGN KEY (persoanl_id) REFERENCES Personal(id))";
+	    			+ "FOREIGN KEY (personal_id) REFERENCES Personal(id))";
 	    	stmt.executeUpdate(sql);
-	    	sql = "CREATE TABLE moegliche_Stundeninhalte_Personal IF NOT EXISTS "
+	    	sql = "CREATE TABLE IF NOT EXISTS moegliche_Stundeninhalte_Personal "
 	    			+ "(personal_id INT NOT NULL, "
 	    			+ "stundeninhalte_id INT NOT NULL, "
-	    			+ ""
+	    			+ "PRIMARY KEY (personal_id, stundeninhalte_id), "
+	    			+ "FOREIGN KEY (personal_id) REFERENCES Personal(id), "
+	    			+ "FOREIGN KEY (stundeninhalte_id) REFERENCES Stundeninhalte(id))";
+	    	stmt.executeUpdate(sql);
+	    	sql = "CREATE TABLE IF NOT EXISTS planungseinheit_Personal "
+	    			+ "(planungseinheit_id INT NOT NULL, "
+	    			+ "personal_id INT NOT NULL, "
+	    			+ "PRIMARY KEY (planungseinheit_id, personal_id), "
+	    			+ "FOREIGN KEY (planungseinheit_id) REFERENCES Planungseinheit(id))";
+	    	stmt.executeUpdate(sql);
+//	    	sql = "CREATE TABLE IF NOT EXISTS klassenlehrer "
+//	    			+ "(schulklasse_id INT NOT NULL, "
+//	    			+ "personal_id INT NOT NULL, "
+//	    			+ "PRIMARY KEY (schulklasse_id, personal"
 	    	stmt.close();
 	    	c.commit();
 	    	c.close();
