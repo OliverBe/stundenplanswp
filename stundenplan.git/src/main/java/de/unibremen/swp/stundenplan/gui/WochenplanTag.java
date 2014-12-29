a5a0958ffa14a010f0739575637cf9e52f8cef59
Paddy <Paddy@Paddys-PC>
2014-12-29 20:13:34 +0100

    Optimierungen

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/WochenplanTag.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/WochenplanTag.java
@@ -1,5 +1,7 @@
 package de.unibremen.swp.stundenplan.gui;
+
 import de.unibremen.swp.stundenplan.*;
+import de.unibremen.swp.stundenplan.config.Config;
 import de.unibremen.swp.stundenplan.data.Personal;
 import de.unibremen.swp.stundenplan.db.Data;
 
@@ -23,34 +25,39 @@ import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.ListModel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 //Diese Klasse repr�sentiert einen Plan und einem bestimmten Tag im Wochenplan.
 public class WochenplanTag extends JPanel {
-	
-	
+
 	private JFileChooser chooser = new JFileChooser();
 	private JFrame f;
-	String[] testeingaben = {"Name 1","Name 2","Name 3","Name 4", "Name 5"};
-	String[][] rowData = { {"","","","","","AA","BB","","","",""}};
+	String[] testeingaben = { "Name 1", "Name 2", "Name 3", "Name 4", "Name 5" };
+	String[][] rowData = { { "", "", "", "", "", "AA", "BB", "", "", "", "" } };
 	public JTable table;
 	public JLabel warning = new JLabel();
 	public DefaultTableModel model = new DefaultTableModel();
 	List<Personal> personalliste = new ArrayList<>();
-	public Personal p1 = new Personal("Testperson 1", "t1", 38, 38, 38, false, true, null);
-	public Personal p2 = new Personal("Testperson 2", "t2", 38, 38, 38, false, true, null);
-	public Personal p3 = new Personal("Testperson 3", "t3", 38, 38, 38, false, true, null);
-	public Personal p4 = new Personal("Testperson 4", "t4", 38, 38, 38, false, true, null);
-	
+	public Personal p1 = new Personal("Testperson 1", "t1", 38, 38, 38, false,
+			true, null);
+	public Personal p2 = new Personal("Testperson 2", "t2", 38, 38, 38, false,
+			true, null);
+	public Personal p3 = new Personal("Testperson 3", "t3", 38, 38, 38, false,
+			true, null);
+	public Personal p4 = new Personal("Testperson 4", "t4", 38, 38, 38, false,
+			true, null);
+
 	public WochenplanTag() {
 		init();
 		addData();
 	}
-	
+
 	public void init(){
 		personalliste.add(p1);
 		personalliste.add(p2);
@@ -66,27 +73,42 @@ public class WochenplanTag extends JPanel {
 		c.gridy = 0;
 		
 		table = new JTable(model);
+		
 		model.addColumn("Personal");
-		model.addColumn("8:00");
-		model.addColumn("9:00");
-		model.addColumn("10:00");
-		model.addColumn("11:00");
-		model.addColumn("12:00");
-		model.addColumn("13:00");
-		model.addColumn("15:00");
-		model.addColumn("16:00");
-		model.addColumn("17:00");
-		model.addColumn("18:00");
+		model.addColumn(Config.DAY_STARTTIME_HOUR+":00");
+		for(int i = (Config.DAY_STARTTIME_HOUR*100); i <=(Config.DAY_ENDTIME_HOUR*100); i+= Config.TIMESLOT_LENGTH){
+			if(i == 60){
+			 i = 0;
+			}
+			int laenge = Integer.toString(i).length();
+			System.out.print(laenge);
+			if(laenge == 3){
+				String text = ""+i;
+				String text2 = text.substring(1,3);
+				String text3 = text.substring(3, text.length());
+			}
+			String text = ""+i;
+			String text2 = text.substring(0,2);
+			String text3 = text.substring(2, text.length());
+			
+			String ausgabe = text2 +":"+text3;
+			model.addColumn(ausgabe);
+			
+		}
 		table.setRowSelectionAllowed(true);
-		table.setRowHeight(30);
+		table.setRowHeight(50);
+		TableColumn column = table.getColumnModel().getColumn(0);
+		 column.setPreferredWidth(100);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		JScrollPane pane = new JScrollPane(table);
-
 		
 		c.fill = GridBagConstraints.BOTH;
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridwidth = 4;
 		c.gridy = 0;
 		c.gridx = 1;
+		
+		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		pane.setPreferredSize(new Dimension(1400, 700));
 		add(pane,c);
 		JButton pdf = new JButton("PDF");
@@ -123,16 +145,15 @@ public class WochenplanTag extends JPanel {
 		
 		
 	}
-	
-	public void addData(){
-		for(Personal e: personalliste){
+
+	public void addData() {
+		for (Personal e : personalliste) {
 
 			String nachname = e.getName();
 			model.addRow(new Object[] { nachname });
 		}
 	}
 
-	
 	private void buttonOkay(JButton b) {
 		b.addActionListener(new ActionListener() {
 			@Override
@@ -140,6 +161,6 @@ public class WochenplanTag extends JPanel {
 				chooser.showSaveDialog(f);
 			}
 		});
-	}	
-	
+	}
+
 }
