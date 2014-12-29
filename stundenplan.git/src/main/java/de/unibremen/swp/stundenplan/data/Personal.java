d7f33dfa608aabf3e3c8ee85fc0c03ffaf76ceb4
Roman <Roman@134.102.232.254>
2014-12-29 15:50:06 +0100

    ad

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/data/Personal.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/data/Personal.java
@@ -20,7 +20,7 @@ import de.unibremen.swp.stundenplan.db.Data;
 
 public final class Personal {
     /**
-     * Der Name dieser LehrerIn.
+     * Der Name dieser Person.
      */
     private String name;
 
