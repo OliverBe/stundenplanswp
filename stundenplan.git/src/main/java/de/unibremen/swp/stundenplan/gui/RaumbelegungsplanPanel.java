a3c6440a715fc4e86f96c25512912dfb529265e8
Fabian <Fabian@134.102.49.63>
2015-01-11 13:47:02 +0100

    Raumbelegungspanel greift auf RamManager hinzu stat direkt auf DB

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/RaumbelegungsplanPanel.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/RaumbelegungsplanPanel.java
@@ -1,41 +1,25 @@
 package de.unibremen.swp.stundenplan.gui;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
-import javax.swing.Box;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.JTable;
-import javax.swing.KeyStroke;
-import javax.swing.ListSelectionModel;
-import javax.swing.SwingConstants;
 
-import de.unibremen.swp.stundenplan.data.Personal;
 import de.unibremen.swp.stundenplan.data.Room;
-import de.unibremen.swp.stundenplan.data.Schoolclass;
-import de.unibremen.swp.stundenplan.data.Stundeninhalt;
-import de.unibremen.swp.stundenplan.db.DataPersonal;
 import de.unibremen.swp.stundenplan.db.DataRaum;
-import de.unibremen.swp.stundenplan.db.DataSchulklasse;
-import de.unibremen.swp.stundenplan.db.DataStundeninhalt;
+import de.unibremen.swp.stundenplan.logic.RaumManager;
 
 public class RaumbelegungsplanPanel extends JPanel implements ActionListener{
 
@@ -100,12 +84,12 @@ public class RaumbelegungsplanPanel extends JPanel implements ActionListener{
 	}
 	
 	public static void updateLists() {
-		Room[] raumListe = new Room[DataRaum.getAllRaum().size()];
+		Room[] raumListe = new Room[RaumManager.getAllRoomsFromDB().size()];
 		rList = new DefaultListModel();
 		
 		
-		for(int i=0; i < DataRaum.getAllRaum().size(); i++) {
-			raumListe[i] = DataRaum.getAllRaum().get(i);
+		for(int i=0; i <RaumManager.getAllRoomsFromDB().size(); i++) {
+			raumListe[i] = RaumManager.getAllRoomsFromDB().get(i);
 
 			rList.addElement(raumListe[i]);
 		}
