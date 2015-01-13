fe569750b79b26d775bae53b75d1c9b8d4966a01
Roman <Roman@Roman-Lap.fritz.box>
2015-01-13 15:17:17 +0100

    edit stundeninhalt drin

---
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/command/EditStundeninhalt.java
@@ -0,0 +1,32 @@
+package de.unibremen.swp.stundenplan.command;
+
+import de.unibremen.swp.stundenplan.data.Room;
+import de.unibremen.swp.stundenplan.data.Stundeninhalt;
+import de.unibremen.swp.stundenplan.db.DataRaum;
+import de.unibremen.swp.stundenplan.db.DataStundeninhalt;
+
+public class EditStundeninhalt implements Command {
+
+	private Stundeninhalt urspruenglich;
+	private Stundeninhalt bearbeitet;
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void execute(String pKuerzel, Stundeninhalt inhalt){
+		urspruenglich = DataStundeninhalt.getStundeninhaltByKuerzel(pKuerzel);
+		bearbeitet = inhalt;
+		DataStundeninhalt.editStundeninhalt(pKuerzel, inhalt);
+		CommandHistory.addCommand(this);
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+		DataStundeninhalt.editStundeninhalt(bearbeitet.getName(), urspruenglich);
+	}
+
+}
