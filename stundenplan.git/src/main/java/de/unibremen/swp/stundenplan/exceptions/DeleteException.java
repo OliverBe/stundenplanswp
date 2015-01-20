bc7f9c20cb2b0d5a8f2c6a96432c06c872930b92
FreakyAsian <k.long24@gmx.de>
2015-01-20 16:44:00 +0100

    DeleteException hinzugefügt

---
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/exceptions/DeleteException.java
@@ -0,0 +1,23 @@
+package de.unibremen.swp.stundenplan.exceptions;
+
+import javax.swing.JOptionPane;
+
+import de.unibremen.swp.stundenplan.Stundenplan;
+
+public class DeleteException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6255145795467711340L;
+	
+	public DeleteException(final String message) {
+        super(message);
+        JOptionPane.showMessageDialog(Stundenplan.getMain(), message);
+    }
+	
+	public static boolean delete(final String message) {
+		int result = JOptionPane.showConfirmDialog(Stundenplan.getMain(), message, "Warnung", JOptionPane.YES_NO_OPTION);
+		System.out.println("DELETEEXCEPTION");
+		return result == JOptionPane.YES_OPTION;
+	}
+}
