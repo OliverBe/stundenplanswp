6ac1e79797be9e7de2337485dbad04dd0fd8176d
FreakyAsian <k.long24@gmx.de>
2015-01-21 16:01:06 +0100

    CommandHistory fix

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/command/CommandHistory.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/command/CommandHistory.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import de.unibremen.swp.stundenplan.exceptions.StundenplanException;
 
 /**
- * Verwaltet die History der ausgeführten Commands. Funktioniert nach dem LIFO-Prinzip.
+ * Verwaltet die History der ausgefï¿½hrten Commands. Funktioniert nach dem LIFO-Prinzip.
  * @author Roman
  *
  */
@@ -13,7 +13,7 @@ public class CommandHistory {
 
 	/**
 	 * Attribut um festzuhalten, ob das letzte Command vom Typ EditCommand ist. Ist im Hinblick
-	 * auf die MainFrame nötig, damit gezielter Updates am Personaleinsatzplan vollzogen werden können.
+	 * auf die MainFrame nï¿½tig, damit gezielter Updates am Personaleinsatzplan vollzogen werden kï¿½nnen.
 	 */
 	private static boolean lastIsEditCommand = false;
 	
@@ -23,11 +23,11 @@ public class CommandHistory {
 	private static ArrayList<Command> commandHistory = new ArrayList<>();
 	
 	/**
-	 * Einfügen eines Commands in die History. Ist das Command vom Typ EditCommand, wird
+	 * Einfï¿½gen eines Commands in die History. Ist das Command vom Typ EditCommand, wird
 	 * das Attribut auf true gesetzt.
 	 * 
 	 * @param c
-	 * 		Das Command, das hinzugefügt werden soll.
+	 * 		Das Command, das hinzugefï¿½gt werden soll.
 	 */
 	public static void addCommand(Command c){
 		commandHistory.add(c);
@@ -35,7 +35,7 @@ public class CommandHistory {
 	}
 	
 	/**
-	 * Gibt das letzte Command aus der History zurück.
+	 * Gibt das letzte Command aus der History zurï¿½ck.
 	 * @return	Das letzte Command, das sich in die History aufgenommen wurde.
 	 * @throws StundenplanException
 	 * 			Wenn sich keine Commands in der History befinden.
@@ -44,24 +44,24 @@ public class CommandHistory {
 		if(commandHistory.size() > 0){ 
 			return commandHistory.get(commandHistory.size() - 1);
 		}else{
-			throw new StundenplanException("Keine Befehle zum rückgängig machen verfügbar.");
+			throw new StundenplanException("Keine Befehle zum rï¿½ckgï¿½ngig machen verfï¿½gbar.");
 		}
 	}
 	
 	/**
-	 * löscht das letzte Command aus der History.
+	 * lï¿½scht das letzte Command aus der History.
 	 */
 	public static void deleteLast(){
 		try{
-			commandHistory.remove(CommandHistory.getLast());
 			if(getLast() instanceof EditCommand) lastIsEditCommand = true;
+			commandHistory.remove(CommandHistory.getLast());
 		}catch(StundenplanException n){
 			System.out.println("[COMMANDHISTORY]: No Command in history yet.");
 		}
 	}
 	
 	/**
-	 * Gibt Attribut zurück, das verwaltet, ob letztes Command ein EditCommand ist.
+	 * Gibt Attribut zurï¿½ck, das verwaltet, ob letztes Command ein EditCommand ist.
 	 * @return
 	 * 		true oder false, wenn Commando ein EditCommand oder nicht
 	 */
