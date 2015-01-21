19c3408bb7e9a762aca18954198ed26c20793b18
Fathan Vidjaja <garila313@hotmail.de>
2015-01-21 14:47:23 +0100

    Noch mehr Restrictions

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/PEedit.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/PEedit.java
@@ -231,6 +231,11 @@ public class PEedit extends JFrame {
 							"Startzeit muss frueher als Endzeit sein");
 					return;
 				}
+				if(p.getEndhour() == TimetableManager.endTimeHour() && p.getEndminute()>TimetableManager.endTimeMinute()){
+					JOptionPane.showMessageDialog(null,
+							"Der Tag ist leider um "+p.getEndhour()+":"+p.getEndminute()+" schon zu Ende.");
+					return;
+				}
 				p.setWeekday((Weekday) tag.getSelectedItem());
 				Iterator it = pList.destinationIterator();
 				ArrayList<Personal> listp = new ArrayList<Personal>();
