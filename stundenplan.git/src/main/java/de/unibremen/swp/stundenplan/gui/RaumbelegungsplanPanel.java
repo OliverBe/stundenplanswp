6678211b9c237ad48350be6e31c9bb15d5c1850a
Fabian <Fabian@Fabs-Home>
2015-01-30 16:31:20 +0100

    WarningPanel im Raumbelegungsplan

--- stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/RaumbelegungsplanPanel.java
+++ stundenplan.git/src/main/java/de/unibremen/swp/stundenplan/gui/RaumbelegungsplanPanel.java
@@ -106,6 +106,10 @@ public class RaumbelegungsplanPanel extends JPanel implements ActionListener {
 		c.weightx = 1.0;
 		c.weighty = 0.2;
 		menuBar.add(show,c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		menuBar.add(new WarningPanel(), c);
 
 	}
 
